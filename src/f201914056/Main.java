6f1f362be2bb741074388673271118f59e55a3ad
Heo, Changhun <powerhch@gmail.com>
2023-10-18 10:55:43 +0900

    four

--- src/f201914056/Main.java
+++ src/f201914056/Main.java
@@ -1,8 +1,11 @@
 package f201914056;
 
 public class Main {
-	public static void main(String[] args) {
-		Two.print();
-		System.out.println("one 201514199");
-	  }
+
+  public static void main(String[] args) {
+    Two.print();
+    System.out.println("one 201514199");
+    Four.print();
+  }
 }
+
